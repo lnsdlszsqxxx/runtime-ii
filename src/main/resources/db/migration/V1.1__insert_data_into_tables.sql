1aa18755f3ab94dc8ef0ced1ef4d7259c6032bab
lnsdlszsqxxx <lyu4@gmu.edu>
2019-08-13 09:51:48 -0400

    change table input from sql script

--- src/main/resources/db/migration/V1.1__insert_data_into_tables.sql
+++ src/main/resources/db/migration/V1.1__insert_data_into_tables.sql
@@ -1,23 +1,23 @@
 insert into department (name, description, location) values
-('HR', 'Human Resource', 'Room 100, 450 N Washington Ave. Falls Church, VA'),
-('R&D', 'Research and Development', 'Room 101, 450 N Washington Ave. Falls Church, VA'),
-('Tech', 'Technical Support', 'Room 102, 450 N Washington Ave. Falls Church, VA'),
-('CS', 'Custom Service', 'Room 103, 450 N Washington Ave. Falls Church, VA');
+('AOES','Atmospheric, Oceanic and Earth Sciences','Research Hall'),
+('GIS','Geography and GeoInformation Science', 'Exploring Hall'),
+('EE','Electronic Engineering','Engineering Building'),
+('CS', 'Computer Science', 'Research Hall');
 
 commit;
 
 insert into employee (name, first_name, last_name, email, address, department_id) values
-('dwang', 'David', 'Wang', 'davey.wang@ascending.com', '123 Fairfax, VA 22030', 2),
-('rhang', 'Ryo', 'Hang', 'ryo.hang@ascending.com', '456 Vienna, VA 22030', 2),
-('gzhang', 'Gloria', 'Zhang', 'gloria.zhang@ascending.com', '789 Dunn Loring, VA 22030', 1),
-('xhuang', 'Xingyue', 'Huang', 'xingyue.Hang@ascending.com', '234 Merrifield, VA 22030', 3);
+('lyu', 'Liang', 'Yu', 'davey.wang@ascending.com', '123 Fairfax, VA 22030', 1),
+('mli', 'Moming', 'Li', 'ryo.hang@ascending.com', '456 Vienna, VA 22030', 2),
+('yli', 'Yun', 'Li', 'gloria.zhang@ascending.com', '789 Dunn Loring, VA 22030', 2),
+('fyang', 'Guangyang', 'Fang', 'xingyue.Hang@ascending.com', '234 Burk, VA 22030', 1);
 
 commit;
 
-insert into account (account_type, balance, employee_id) values
-('checking', 99999999.99, 1),
-('saving', 99999999.99, 2),
-('checking', 99999999.99, 3),
-('saving', 99999999.99, 4);
+insert into account(account_type, balance, employee_id) values
+('checking', 123.23, 1),
+('saving', 234.34, 2),
+('checking', 345.45, 3),
+('saving', 456.56, 4);
 
 commit;
